0027b076ebad863cbd76ebfd6d4b95477e8bd4e3
Wacoder Forever <sethomondi088@gmail.com>
2024-04-30 20:25:04 +0300

    tftfgh

--- main.c
+++ main.c
@@ -1,57 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-#include "card/card.h"
-#include "deck/deck.h"
-
-#include "card/card.c"
-#include "deck/deck.c"
+#include "decklib.h"
 
 int main(){
 
-    srand(time(NULL));
-
-    Card *card1=newCard("Spade","Ace");
-    Card *card2=newCard("Diamond","2");
-    Card *card3=newCard("Clubs","3");
-    Card *card4=newCard("Hearts","4");
-    Card *card5=newCard("Spade","5");
-    Card *card6=newCard("Spade","Ace");
-
-    Deck *maindeck=newDeck();
-    AddCardToDeck(maindeck,card1);
-    AddCardToDeck(maindeck,card2);
-    AddCardToDeck(maindeck,card3);
-    AddCardToDeck(maindeck,card4);
-    AddCardToDeck(maindeck,card5);
-
-    printf("Printing cards added to the deck:\n");
-    for(int i=0;i<maindeck->size;i++){
-
-        Card *temp=maindeck->cards[i];
-        printf("%7s     %s\n",GetCardSuit(temp),GetCardNumber(temp));
-    }
-
-    Deck *player=DealCards(maindeck);
-
-    printf("Printing cards dealt to player:\n");
-    for(int i=0;i<player->size;i++){
-
-        Card *temp=player->cards[i];
-        printf("%7s     %s\n",GetCardSuit(temp),GetCardNumber(temp));
-    }
-
-    printf("Printing cards remaining on deck:\n");
-    for(int i=0;i<maindeck->size;i++){
-
-        Card *temp=maindeck->cards[i];
-        printf("%7s     %s\n",GetCardSuit(temp),GetCardNumber(temp));
-    }
-
-    FreeDeck(maindeck);
-    FreeDeck(player);
-
+    printf("hello world\n");
     return 0;
 }
